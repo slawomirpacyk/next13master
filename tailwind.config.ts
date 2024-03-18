1f409acc3f7d74d222699f88c5fcf3877e45345c
slawomir.pacyk <slawomir.pacyk@gmail.com>
2024-03-18 18:18:42 +0100

    chapter 1 / commit with fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
 	content: [
 		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
@@ -16,5 +16,4 @@ const config: Config = {
 	},
 	plugins: [],
 };
-
 export default config;
