8a6d9899f879884929947d84afa5fd84c573379d
slawomir.pacyk <slawomir.pacyk@gmail.com>
2024-03-18 18:30:52 +0100

    chapter 1 / product images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const products: ProductListItemTypes[] = [
 		name: "Chanel",
 		price: 1200,
 		coverImage: {
-			src: "https://images.unsplash.com/photo-1563170351-be82bc888aa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=376&q=80",
+			src: "/photo-1563170351-be82bc888aa4.avif",
 			alt: "Chanel perfume",
 		},
 	},
@@ -18,7 +18,7 @@ const products: ProductListItemTypes[] = [
 		name: "Man Mix",
 		price: 1200,
 		coverImage: {
-			src: "https://images.unsplash.com/photo-1544441893-675973e31985?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+			src: "/photo-1544441893-675973e31985.avif",
 			alt: "Man Mix clothes",
 		},
 	},
@@ -28,7 +28,7 @@ const products: ProductListItemTypes[] = [
 		name: "Classic Watch",
 		price: 1200,
 		coverImage: {
-			src: "https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+			src: "/photo-1532667449560-72a95c8d381b.avif",
 			alt: "Classic Watch",
 		},
 	},
@@ -38,7 +38,7 @@ const products: ProductListItemTypes[] = [
 		name: "Woman Mix",
 		price: 1200,
 		coverImage: {
-			src: "https://images.unsplash.com/photo-1590664863685-a99ef05e9f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=345&q=80",
+			src: "/photo-1590664863685-a99ef05e9f61.avif",
 			alt: "Woman Mix clothes",
 		},
 	},
